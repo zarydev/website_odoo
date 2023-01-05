e6bc3a3210b4f9c462dbf969c02dadf8ce53a02d
zarydev <rhapsodyred@live.com.mx>
2023-01-05 01:42:13 +0000

    Se corrige error nuevamente

--- myweb/addons/myweb/controllers/main.py
+++ myweb/addons/myweb/controllers/main.py
@@ -6,5 +6,4 @@ class WsController(http.Controller):
         '/mywebsite',
         ], type='http', auth='public', website=True, cors='*')
     def main(self):
-
         return request.render('myweb.website',{})
