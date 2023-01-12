7aabf379ea080528212eb3e49e62c7789a4423a3
zarydev <rhapsodyred@live.com.mx>
2023-01-12 02:07:38 +0000

    Se integra inser a CRM

--- myweb/addons/myweb/controllers/main.py
+++ myweb/addons/myweb/controllers/main.py
@@ -7,3 +7,7 @@ class WsController(http.Controller):
         ], type='http', auth='public', website=True, cors='*')
     def main(self):
         return request.render('myweb.website',{})
+
+    @http.route('/create/crmclient', type="http", auth="public", website=False)
+    def create_crmclient(self, **kw):
+        request.env['crm.lead'].sudo().create(kw)
